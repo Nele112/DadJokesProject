91029acba7cfa9b99d97805e7ce82bcabad29e0a
Nele112 <nen00013@laurea.fi>
2025-05-24 13:36:52 +0300

    Fix: corrected header X-Api-Key syntax

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 exports.handler = async function(event, context) {
   const response = await fetch("https://api.api-ninjas.com/v1/dadjokes", {
     headers: {
-      "X-Api_Key": process.env.API_NINJAS_KEY
+      "X-Api-Key": process.env.API_NINJAS_KEY
     }
   });
 
